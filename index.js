776b2338659cd6842ef0dcd3e7b7662468305c08
axiomaabsurdo <m.mortara01@gmail.com>
2020-08-15 21:16:07 -0300

    fix: view folder edge engine

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 "use strict"
 
 const path = require('path');
-const expressEdge = require('express-edge');
+// const expressEdge = require('express-edge');
+const { config, engine } = require('express-edge');
 const express = require('express');
 const app = new express();
 
 app.use(express.static('public'));
-app.use(expressEdge.engine);
-app.set('views', __dirname + '/views');
+app.use(engine);
+
+app.set('views', `${__dirname}/views`);
+// app.set("views", path.resolve(__dirname, "views"));
+// app.set('views', path.join(__dirname, 'views'));
+// console.log( `Concha de tu madre ${__dirname}` )
 
 
 //GET INDEX PAGE AND LOAD IT
@@ -15,7 +20,8 @@ app.set('views', __dirname + '/views');
 //     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
 // });
 app.get('/index', (req, res) => {
-    res.render('/index');
+    // console.log( `Concha de tu madre 2 ${__dirname}` )
+    res.render('index');
 });
 
 //GET ABOUT PAGE AND LOAD IT
